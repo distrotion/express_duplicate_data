e427892248448a7f211f8fa0fa8890a1a2c24ede
Sittichok Hongthong <sittichok.ho@gmail.com>
2021-02-03 19:54:27 +0700

    .

--- index.js
+++ index.js
@@ -20,12 +20,48 @@ app.post('/', (req, res) => {
         PO_list[i] =  input[i]['PO'];
         CUSLOTNO_list[i] =  input[i]['CUSLOTNO'];
     }
-    
 
+    dup_cout = [];
+    dc = [];
+    dup=[]
+    for(i=0;i<PO_list.length;i++){
+        dc[i] = 0;
+        dup[i] =[]
+        for(j=0;j<input.length;j++){
+            
+            if(PO_list[i]===input[j]['PO']){
+                //console.log(`${i}:${j}:${input[j]['PO']}`)
+                dup[i][dc[i]]=input[j]['CUSLOTNO']
+                dc[i]++;
+            }
+
+        }
+    }
+    PO_list_dup = [...new Set(PO_list)]
+    dup_all = [...new Set(dup)]
+
+    output = []
+    for(i=0;i<PO_list_dup.length;i++){
+        for(j=0;j<input.length;j++){
+            if(PO_list_dup[i]===input[j]['PO']){
+                output[i]=input[j]
+                for(k=0;k<PO_list.length;k++){
+                    if(PO_list[k]===PO_list_dup[i]){
+                        output[i]['CUSLOTNO']=`${dup[k]}`;
+                        break;
+                    }
+                    
+                }
+                break;
+            }
+            
+        }
+    }
+        
     //console.log(input)
     //comon_res = {"massage":'KUY PARIN FROM POST'}
-    res.json(CUSLOTNO_list);
+    res.json(output);
 });
-
+    
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port${port}...`) );
